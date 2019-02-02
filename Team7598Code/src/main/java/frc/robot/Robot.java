519ef14db9c48b1c29db792e6f1167e85e998a0a
NAMITHAJOHN <njohn@scsacademy.org>
2019-02-02 10:44:33 -0500

    Added variables for GamePad

--- Team7598Code/src/main/java/frc/robot/Robot.java
+++ Team7598Code/src/main/java/frc/robot/Robot.java
@@ -14,6 +14,7 @@ import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import frc.robot.commands.ExampleCommand;
 import frc.robot.subsystems.ExampleSubsystem;
+import frc.robot.subsystems.UltrasonicSensor;
 
 /**
  * The VM is configured to automatically run this class, and to call the
@@ -25,6 +26,7 @@ import frc.robot.subsystems.ExampleSubsystem;
 public class Robot extends TimedRobot {
   public static ExampleSubsystem m_subsystem = new ExampleSubsystem();
   public static OI m_oi;
+  public static UltrasonicSensor m_ultraSens;
 
   Command m_autonomousCommand;
   SendableChooser<Command> m_chooser = new SendableChooser<>();
@@ -36,6 +38,7 @@ public class Robot extends TimedRobot {
   @Override
   public void robotInit() {
     m_oi = new OI();
+    m_ultraSens = new UltrasonicSensor();
     m_chooser.setDefaultOption("Default Auto", new ExampleCommand());
     // chooser.addOption("My Auto", new MyAutoCommand());
     SmartDashboard.putData("Auto mode", m_chooser);
--- Team7598Code/src/main/java/frc/robot/RobotMap.java
+++ Team7598Code/src/main/java/frc/robot/RobotMap.java
@@ -18,9 +18,41 @@ public class RobotMap {
   // following variables to use with your drivetrain subsystem.
   // public static int leftMotor = 1;
   // public static int rightMotor = 2;
+public static int fowardDoubleSolenoid =  0;
+public static int reverseDoubleSolenoid = 1;
+
+//ultrasonic sensor port
+public static int ultrasonicSensorPort = 0;
+
+//gamepad axis ports
+public static int AXISLEFTX = 1;
+public static int AXISLEFTY = 2;
+public static int AXISSHOULDER = 3;
+public static int AXISRIGHTX = 4;
+public static int axisRightY = 5;
+public static int axisDPad = 6;
+
+//gamepad buttons
+public static int buttonA = 2;
+public static int buttonB = 3;
+public static int buttonX = 1;
+public static int buttonY = 4;
+public static int buttonShoulderL = 5;
+public static int buttonShoulderR = 6;
+public static int buttonTriggerL = 7;
+public static int buttonTriggerR = 8;
+public static int buttonBack = 9;
+public static int buttonStart = 10;
+public static int buttonStickL = 11;
+public static int buttonStickR = 12;
+public static int buttonMode = -1;
+public static int buttonLogitech  = -1;
 
   // If you are using multiple modules, make sure to define both the port
   // number and the module. For example you with a rangefinder:
   // public static int rangefinderPort = 1;
   // public static int rangefinderModule = 1;
+
+  
+ 
 }
