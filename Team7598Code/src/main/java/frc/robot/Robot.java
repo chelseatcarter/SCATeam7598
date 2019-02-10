806fb5ece3835be51899e563bfb4dad9791aceb7
Chelsea Carter <chelseatcarter@gmail.com>
2019-02-09 21:58:22 -0500

    Removing files that are not necessary to use the encoder

--- Team7598Code/src/main/java/frc/robot/Robot.java
+++ Team7598Code/src/main/java/frc/robot/Robot.java
@@ -11,8 +11,6 @@ import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj.command.Command;
 import edu.wpi.first.wpilibj.command.Scheduler;
 import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
-import frc.robot.subsystems.UltrasonicSensor;
-import frc.robot.subsystems.Drive;
 /**
  * The VM is configured to automatically run this class, and to call the
  * functions corresponding to each mode, as described in the TimedRobot
@@ -21,9 +19,6 @@ import frc.robot.subsystems.Drive;
  * project.
  */
 public class Robot extends TimedRobot {
-  public static Drive m_drive = new Drive();
-  public static OI m_oi;
-  public static UltrasonicSensor m_ultraSens;
 
   Command m_autonomousCommand;
   SendableChooser<Command> m_chooser = new SendableChooser<>();
@@ -34,8 +29,6 @@ public class Robot extends TimedRobot {
    */
   @Override
   public void robotInit() {
-    m_oi = new OI();
-    m_ultraSens = new UltrasonicSensor();
   }
 
   /**
